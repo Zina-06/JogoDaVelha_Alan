18ffe791865bc73032a376c421f075a4920fc2e4
Fernando Ribeiro <fernandoearibeiro8@gmail.com>
2024-03-29 18:57:16 -0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JogoDaVelha_Alan
 {
@@ -10,30 +7,45 @@ namespace JogoDaVelha_Alan
     {
         static void Main(string[] args)
         {
+            bool jogarNovamente = true;
+
+            while (jogarNovamente)
+            {
+                JogoDaVelha();
+
+                Console.Write("\nDeseja jogar novamente? (s/n): ");
+                char resposta = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (resposta != 's' && resposta != 'S')
+                    jogarNovamente = false;
+
+                Console.Clear();
+            }
+
+            Console.WriteLine("Obrigado por jogar!");
+            Console.ReadKey();
+        }
+
+        static void JogoDaVelha()
+        {
             string[,] matriz = new string[3, 3];
             string turno = "X";
-            List<string> indexNumeros = new List<string> { };
+            List<string> indexNumeros = new List<string>();
             int index = 1;
-            int tentativas = 0;
+            int tentativas = 1;
+            menuInicio();
+            string jogada;
 
-            //Menu de início
-            Console.WriteLine("===========================================");
-            Console.WriteLine("              JOGO DA VELHA");
-            Console.WriteLine("      Feito por: Fernando 1º Termo");
-            Console.WriteLine("===========================================");
-            Console.ReadKey();
-            Console.Clear();
-
-            //Pergunta o nome dos jogadores
+            // Pergunta o nome dos jogadores
             Console.Write("Jogador 1: ");
             string jogador1 = Console.ReadLine();
             Console.Write("Jogador 2: ");
             string jogador2 = Console.ReadLine();
-
             string jogador = jogador1;
             Console.Clear();
 
-            //Alimentando a matriz
+            // Alimentando a matriz
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
                 for (int j = 0; j < matriz.GetLength(1); j++)
@@ -44,7 +56,7 @@ namespace JogoDaVelha_Alan
                 }
             }
 
-            //Lendo a matriz
+            // Lendo a matriz
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
                 for (int j = 0; j < matriz.GetLength(1); j++)
@@ -54,20 +66,11 @@ namespace JogoDaVelha_Alan
                 Console.WriteLine();
             }
 
-            //Pergunta a posição que o jogador quer jogar
-            Console.Write($"Escolha a posição, {jogador}({turno}): ");
-            string jogada = Console.ReadLine();
-            while (!indexNumeros.Contains(jogada))
-            {
-                Console.Write("Jogada inválida. Tente novamente: ");
-                jogada = Console.ReadLine();
-            }
-            Console.Clear();
+            // Pergunta a posição que o jogador quer jogar
+            jogada = jogadaCheck(turno, indexNumeros, jogador);
 
-
-            //Começo do jogo
-
-            while (tentativas < 9)
+            // Começo do jogo
+            while (tentativas < 10)
             {
                 // Substitui o valor em sua respectiva casa
                 for (int i = 0; i < matriz.GetLength(0); i++)
@@ -91,24 +94,21 @@ namespace JogoDaVelha_Alan
                     }
                     Console.WriteLine();
                 }
-
+                
                 // Checagem de vitória nas diagonais
                 if ((matriz[0, 0] == matriz[1, 1] && matriz[1, 1] == matriz[2, 2]) ||
                     (matriz[0, 2] == matriz[1, 1] && matriz[1, 1] == matriz[2, 0]))
                 {
-                    Console.WriteLine("------------");
-                    Console.WriteLine("Fim de jogo!");
-                    Console.WriteLine("------------");
-                    Console.WriteLine($"\nParabéns! O vencedor é: {jogador}({turno})");
-                    break;
+                    vitoria(turno, jogador);
+                    return;
                 }
 
                 // Checagem de vitória nas linhas
                 for (int i = 0; i < 3; i++)
                 {
-                    if ((matriz[i, 0] == matriz[i, 1] && matriz[i, 1] == matriz[i, 2]))
+                    if (matriz[i, 0] == matriz[i, 1] && matriz[i, 1] == matriz[i, 2])
                     {
-                        Console.WriteLine("vitória");
+                        vitoria(turno, jogador);
                         return;
                     }
                 }
@@ -116,47 +116,68 @@ namespace JogoDaVelha_Alan
                 // Checagem de vitória nas colunas
                 for (int j = 0; j < 3; j++)
                 {
-                    if ((matriz[0, j] == matriz[1, j] && matriz[1, j] == matriz[2, j]))
+                    if (matriz[0, j] == matriz[1, j] && matriz[1, j] == matriz[2, j])
                     {
-                        Console.WriteLine("vitória");
+                        vitoria(turno, jogador);
                         return;
                     }
                 }
 
-                tentativas++;
+                    tentativas++;
 
                 // Alternância de turnos
-                if (turno == "X")
-                {
-                    turno = "O";
-                }
-                else
-                {
-                    turno = "X";
-                }
+                altTurnos(ref turno, jogador1, jogador2, ref jogador);
 
-                if (jogador == jogador1)
-                {
-                    jogador = jogador2;
-                }
-                else
-                {
-                    jogador = jogador1;
-                }
+                // Checa se a jogada é válida
+                jogada = jogadaCheck(turno, indexNumeros, jogador);
 
-                Console.Write($"Escolha a posição, {jogador}({turno}): ");
-                jogada = Console.ReadLine();
-
-                while (!indexNumeros.Contains(jogada))
+                if(tentativas == 9)
                 {
-                    Console.Write("Jogada inválida. Tente novamente: ");
-                    jogada = Console.ReadLine();
+                    Console.WriteLine("Empato");
                 }
-
-                Console.Clear();
             }
+            
+        }
+
+        private static void altTurnos(ref string turno, string jogador1, string jogador2, ref string jogador)
+        {
+            turno = (turno == "X") ? "O" : "X";
+            jogador = (jogador == jogador1) ? jogador2 : jogador1;
+        }
+
+        private static string jogadaCheck(string turno, List<string> indexNumeros, string jogador)
+        {
+            string jogada;
+            Console.Write($"Escolha a posição, {jogador}({turno}): ");
+            jogada = Console.ReadLine();
+
+            while (!indexNumeros.Contains(jogada))
+            {
+                Console.Write("Jogada inválida. Tente novamente: ");
+                jogada = Console.ReadLine();
+            }
+            Console.Clear();
+            return jogada;
+        }
+
+        private static void vitoria(string turno, string jogador)
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine("Fim de jogo!");
+            Console.WriteLine("------------");
+            Console.WriteLine($"\nParabéns! O vencedor é: {jogador}({turno})");
+        }
+
+        private static void menuInicio()
+        {
+
+            //Menu de início
+            Console.WriteLine("===========================================");
+            Console.WriteLine("              JOGO DA VELHA");
+            Console.WriteLine($"      Feito por: Fernando 1° Termo");
+            Console.WriteLine("===========================================");
             Console.ReadKey();
+            Console.Clear();
         }
     }
-
-    }
+}
